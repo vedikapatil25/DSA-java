a42c781f99893454fbe1e9d16c6b00a4cbad44b7
vedika <vedikapatil88888@gmail.com>
2026-09-14 17:25:09 +0530

    Added new DSA-problem

---
+++ Basics/CountEvenOddDigits.java
@@ -0,0 +1,23 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class CountEvenOddDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int evenCount = 0;
+        int oddCount =  0;
+        while(n>0){
+            int lastDigit = n % 10;
+            if(lastDigit % 2 == 0){
+                evenCount++;
+            }
+            else{
+                oddCount++;
+            }
+            n = n/10;
+        }
+        System.out.println("Even digits are :" + evenCount + " Odd digits are :" + oddCount);
+    }
+}
