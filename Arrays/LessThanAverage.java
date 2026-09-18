b3de7115a67643104abd810e6adb1e8ceaa7c1b9
vedika <vedikapatil88888@gmail.com>
2026-09-18 17:11:10 +0530

    Added new DSA-problem

---
+++ Arrays/LessThanAverage.java
@@ -0,0 +1,20 @@
+public class LessThanAverage {
+    public static void main(String[] args) {
+        int arr[] = {75, 45,  28 , 19 , 52, 61};
+        int sum = 0;
+        for(int i=0; i<arr.length;i++){
+            sum += arr[i];
+        }
+        double avg = (double)sum/arr.length;
+        int count = 0;
+        System.out.println("average is: " + avg);
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<avg){
+                count++;
+            }
+        }
+        System.out.println("Average count is :" + count);
+    }
+    
+}
+
