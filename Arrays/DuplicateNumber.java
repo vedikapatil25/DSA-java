13f8dd12690473311a272b032735572287d22f87
vedika <vedikapatil88888@gmail.com>
2026-09-22 23:40:09 +0530

    Added new DSA-problem

---
+++ Arrays/DuplicateNumber.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class DuplicateNumber {
+
+    public static void main(String[] args) {
+
+        int arr[] = {24, 12, 65, 87, 12, 90, 12};
+        HashSet<Integer> set = new HashSet<>(); 
+
+        for (int i = 0; i < arr.length; i++) {
+                if (set.contains(arr[i])) {
+                    System.out.print("Duplicate number: " + arr[i]);
+                    break;
+                }
+                set.add(arr[i]);
+        }
+    }
+}
